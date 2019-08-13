eb9e2155cf4687e938a93d9ae69807adb35f2e36
wilsonalexandrebaez <wab.estacio@gmail.com>
2019-08-13 09:43:51 -0400

    atualização de arquivos

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-const {app, BrowserWindow } = require('electron');
+const {app,BrowserWindow} = require('electron');
 
-function createwindow(){
-    let mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
-    })
-    mainWindow.loadFile(__dirname + '/src/index.html');
-    mainWindow.on('closed', function (){
-        mainWindow = null;
-    })
- }
- app.on('ready', createwindow);
+let mainWindow = null;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({width:800, height:600});
+  mainWindow.loadURL ('file://' + __dirname + '/index.html');
+
+  mainWindow.webContents.openDevTools();
+});
